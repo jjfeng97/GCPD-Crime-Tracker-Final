a7fecc82b00c1107528a563c43052a4337508fad
Joie Feng <jjfeng@andrew.cmu.edu>
2018-11-29 17:56:50 -0500

    created recent investigations module for commish dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,14 @@ class DashboardController < ApplicationController
 
   def index
   	@officer = current_user.officer
-  	@current_assignments = @officer.assignments.current.chronological.to_a.reverse
-    @past_assignments = @officer.assignments.past.chronological.to_a.reverse
+  	if current_user.role == "officer"
+  		@current_assignments = @officer.assignments.current.chronological.to_a.reverse
+    	@past_assignments = @officer.assignments.past.chronological.to_a.reverse
+    elsif current_user.role == "commish"
+    	@recent_investigations = Investigation.is_open.chronological.to_a.reverse.slice(0, 5)
+    	@recent_assignments = Assignment.current.chronological.to_a.reverse.slice(0, 5)
+    	@recent_suspects = Suspect.current.chronological.to_a.reverse.slice(0, 5)
+    end
   end
 
   
