76df63ea68f1dce6251756ccff81dd640793ac77
Joie Feng <jjfeng@andrew.cmu.edu>
2018-12-03 17:31:10 -0500

    changed some css stuff

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -28,7 +28,7 @@ class CrimesController < ApplicationController
     @crime = Crime.find(params[:id])
     respond_to do |format|
       if @crime.update_attributes(crime_params)
-        format.html { redirect_to @crime, notice: "Successfully updated information for #{@crime.name}." }
+        format.html { redirect_to crimes_path, notice: "Successfully updated information for #{@crime.name}." }
 
       else
         format.html { render :action => "edit" }
